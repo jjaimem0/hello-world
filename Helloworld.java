4f1e287d87453f78ea36c22bd1462fdbb2545b2d
jjaimem0 <jjaimem0@email.cpcc.edu>
2021-02-02 18:41:28 -0500

    george washington
    added a Sting and an input

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,17 @@ public class Helloworld
 {
    public static void main (String[] args)
    {
-      System.out.println("Hello World!")
-   }
-}
+      System.out.println("Hello World!");
+      
+      //name is a reference variable
+      String name = "George Washington";
+      
+      System.out.println(name);
+      
+      //age is a primitive variable
+      int age = 24;
+      
+      System.out.println(age);
+      
+   } // end main
+} // ends class
